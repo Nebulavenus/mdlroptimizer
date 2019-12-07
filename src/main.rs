4cb6d1a37d2a2cf8f2b4da6351d562c4510e53cc
Nebula Venus <6595784+Nebulavenus@users.noreply.github.com>
2019-12-07 23:55:47 +0600

    Pest dependency

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+extern crate clap;
+
+use clap::{App, Arg, ArgMatches, SubCommand};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("MdlROptimizer")
+        .version("0.1")
+        .about("Tool for optimizing mdl models.")
+        .author("Nebula Venus (Github)")
+        .arg(Arg::with_name("dir")
+            .help("Process all files in directory")
+            .takes_value(true)
+            .short("p")
+            .short("parse-all"))
+        .get_matches();
+
+    if let Some(dir) = matches.value_of("dir") {
+        // Logic
+    }
 }
