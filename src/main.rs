f0f0c110a740b979b3118000543cb429f73d439c
Nebula Venus <6595784+Nebulavenus@users.noreply.github.com>
2019-12-17 01:40:01 +0600

    Testing simple cli interface

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+extern crate took;
 extern crate clap;
 extern crate pest;
 extern crate regex;
@@ -5,6 +6,8 @@ extern crate regex;
 extern crate pest_derive;
 #[macro_use]
 extern crate lazy_static;
+#[macro_use]
+extern crate took_macro;
 
 use clap::{App, Arg, ArgMatches, SubCommand};
 
@@ -17,10 +20,21 @@ use parser::parse_file;
 use optimizer::optimize_model;
 use crate::util::{remove_redundant_lines, replace_values_at_spans};
 use std::fs;
+use std::path::Path;
+use std::fs::File;
+use std::io::Read;
 
-pub fn parse_optimize_model(path: &str) {
-    let raw_file = fs::read_to_string(path).expect("cannot read file");
-    let (file, model) = parse_file(&raw_file);
+// TODO(nv): rewrite this in future
+#[took(description = "done model optimizing in")]
+pub fn parse_optimize_model(path: &Path) {
+    let file_name = path.file_stem().unwrap();
+    let mut file = File::open(path).expect("cannot find file");
+    let buf_size = file.metadata().map(|m| m.len() as usize + 1).unwrap_or(0);
+    let mut raw_string = String::with_capacity(buf_size);
+    file.read_to_string(&mut raw_string).expect("good");
+    //let raw_file = fs::read_to_string(path).expect("cannot read file");
+
+    let (file, model) = parse_file(&raw_string);
     let (redundant_lines, _, _) = optimize_model(model);
     let file1 = remove_redundant_lines(file.clone(), redundant_lines);
 
@@ -28,26 +42,38 @@ pub fn parse_optimize_model(path: &str) {
     let (file2, model1) = parse_file(&file1);
     let (_, translation_spans, _) = optimize_model(model1);
     let file3 = replace_values_at_spans(file2, translation_spans);
+
+    let (file4, model2) = parse_file(&file3);
+    let (_, _, rotation_spans) = optimize_model(model2);
+    let file5 = replace_values_at_spans(file4, rotation_spans);
     //let (result, _) = replace_values_at_spans(f2, rs, d2);
 
-    std::fs::write("test1.mdl", file);
-    std::fs::write("test.mdl", file3);
+    let new_file_name =
+        String::from(file_name.to_str().unwrap()) + String::from("_optimized.mdl").as_ref();
+
+    std::fs::write(new_file_name, file5);
 }
 
 fn main() {
-    let matches = App::new("Mdlroptimizer")
-        .version("0.1")
-        .about("Tool for optimizing mdl models.")
-        .author("Nebula Venus (Github)")
+    let matches = App::new("   Mdlroptimizer")
+        .version("0.1.0")
+        .about("   Tool for optimizing mdl models.")
+        .author("   Nebula Venus (Github)")
+        /*
         .arg(Arg::with_name("dir")
             .help("Process all files in directory")
             .takes_value(true)
             .short("p")
             .short("parse-all"))
+        */
+        .arg(Arg::with_name("file")
+            .help("Optimize mdl file")
+            .takes_value(true)
+            .long("optimize"))
         .get_matches();
 
-    if let Some(dir) = matches.value_of("dir") {
-        // Logic
+    if let Some(file) = matches.value_of("file") {
+        parse_optimize_model(file.as_ref());
     }
 }
 
@@ -57,6 +83,6 @@ mod tests {
 
     #[test]
     fn test() {
-        parse_optimize_model("././testfiles/ChaosWarrior_opt1.mdl");
+        parse_optimize_model("././testfiles/ChaosWarrior_opt1.mdl".as_ref());
     }
 }
